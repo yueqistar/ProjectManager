c52c9735983b34a70f413ab90eb9d8c57dffbd8b
yueqistar <2775143912@qq.com>
2016-09-12 15:30:31 +0800

    add VCRoot

---
+++ Test63/VCRoot.h
@@ -0,0 +1,13 @@
+//
+//  VCRoot.h
+//  Test63
+//
+//  Created by 越七 on 16/9/12.
+//  Copyright © 2016年 YueQi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VCRoot : UIViewController
+
+@end
